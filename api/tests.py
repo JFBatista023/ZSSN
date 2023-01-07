209dc92c0a30d4b83079231d9a96d4954f6e934a
Filipe <joaofilipeb045@gmail.com>
2023-01-06 23:54:11 -0300

    Finishing test for create new survivor

--- api/tests.py
+++ api/tests.py
@@ -5,7 +5,17 @@ from api.models import Survivor, Inventory, QuantityItem, Item
 
 
 class SurvivorTests(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.water = Item.objects.create(name='water', points='4')
+        cls.food = Item.objects.create(name='food', points='3')
+        cls.medication = Item.objects.create(
+            name='medication', points='2')
+        cls.ammo = Item.objects.create(name='ammo', points='1')
+
     def test_create_new_survivor(self):
+        Survivor.objects.all().delete()
         url = reverse("api:survivor-list")
         data = {
             "survivor": {
@@ -25,15 +35,15 @@ class SurvivorTests(APITestCase):
             }
         }
 
-        print(url)
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Survivor.objects.count(), 1)
         self.assertEqual(Survivor.objects.get().name, 'Filipe Batista')
+        self.assertEqual(Survivor.objects.get(),
+                         Inventory.objects.get().survivor)
         self.assertFalse(Survivor.objects.get().is_infected)
         for item in QuantityItem.objects.all():
-            self.assertEqual(item.quantity, 2)
-            self.assertEqual(item.quantity, 5)
-            self.assertEqual(item.quantity, 2)
-
-        Survivor.objects.all().delete()
+            if item.item.name == 'water' or item.item.name == 'medication':
+                self.assertEqual(item.quantity, 2)
+            if item.item.name == 'food':
+                self.assertEqual(item.quantity, 5)
